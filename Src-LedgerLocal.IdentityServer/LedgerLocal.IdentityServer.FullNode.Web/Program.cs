bef742027b048b378709d085dabbe425c47655a5
alexandre.lazar <alexandre.lazar@gmail.com>
2018-04-16 09:40:25 +0200

    fix identity2

--- Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Program.cs
+++ Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Program.cs
@@ -3,23 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace LedgerLocal.IdentityServer.FullNode.Web
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
                 .Build();
-
-            host.Run();
-        }
     }
 }
