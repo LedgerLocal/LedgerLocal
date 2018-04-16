c73700f3fb9f811e2a935e59452092d16c4ac025
alexandre.lazar <alexandre.lazar@gmail.com>
2018-04-16 13:15:33 +0200

    fix cors

--- Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Startup.cs
+++ Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Startup.cs
@@ -17,6 +17,7 @@ using LedgerLocal.IdentityServer.FullNode.Web.Services;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.DataProtection;
 using System.IO;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 
 namespace LedgerLocal.IdentityServer.FullNode.Web
 {
@@ -100,7 +101,20 @@ namespace LedgerLocal.IdentityServer.FullNode.Web
                     options.TokenCleanupInterval = 30;
                 });
 
-            services.AddCors();
+            // ********************
+            // Setup CORS
+            // ********************
+            var corsBuilder = new CorsPolicyBuilder();
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowAnyMethod();
+            corsBuilder.AllowAnyOrigin(); // For anyone access.
+            //corsBuilder.WithOrigins("http://localhost:56573"); // for a specific url. Don't add a forward slash on the end!
+            corsBuilder.AllowCredentials();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
+            });
 
             services.AddMvc();
 
