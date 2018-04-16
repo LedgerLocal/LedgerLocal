b9f258e96b6d86abd721dedaf1faafc5f8da8322
alexandre.lazar <alexandre.lazar@gmail.com>
2018-04-16 21:40:06 +0200

    fix auth

--- Src-LedgerLocal.FrontServer.Web/LedgerLocal.FrontServer.Front.AngularWeb/ClientApp/src/app/app.module.ts
+++ Src-LedgerLocal.FrontServer.Web/LedgerLocal.FrontServer.Front.AngularWeb/ClientApp/src/app/app.module.ts
@@ -19,11 +19,6 @@ import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
 import { ContentService } from './@core/data/contentservice';
 
-export function loadConfig(oidcConfigService: OidcConfigService) {
-  console.log('APP_INITIALIZER STARTING');
-  return () => oidcConfigService.load_using_custom_stsServer('https://identity.ledgerlocal.com/.well-known/openid-configuration');
-}
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -41,12 +36,6 @@ export function loadConfig(oidcConfigService: OidcConfigService) {
   bootstrap: [AppComponent],
   providers: [
     OidcConfigService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: loadConfig,
-      deps: [OidcConfigService],
-      multi: true
-    },
     { provide: APP_BASE_HREF, useValue: '/' },
     ContentService,
   ],
@@ -57,40 +46,43 @@ export class AppModule {
   constructor(private oidcSecurityService: OidcSecurityService,
     private oidcConfigService: OidcConfigService) {
 
-    this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+    const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
 
-      const openIDImplicitFlowConfiguration = new OpenIDImplicitFlowConfiguration();
-      openIDImplicitFlowConfiguration.stsServer = this.oidcConfigService.clientConfiguration.stsServer;
-      openIDImplicitFlowConfiguration.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
-      // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer
-      // identified by the iss (issuer) Claim as an audience.
-      // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience,
-      // or if it contains additional audiences not trusted by the Client.
-      openIDImplicitFlowConfiguration.client_id = this.oidcConfigService.clientConfiguration.client_id;
-      openIDImplicitFlowConfiguration.response_type = this.oidcConfigService.clientConfiguration.response_type;
-      openIDImplicitFlowConfiguration.scope = this.oidcConfigService.clientConfiguration.scope;
-      openIDImplicitFlowConfiguration.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
-      openIDImplicitFlowConfiguration.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
-      openIDImplicitFlowConfiguration.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
-      openIDImplicitFlowConfiguration.silent_renew_url = this.oidcConfigService.clientConfiguration.silent_renew_url;
-      openIDImplicitFlowConfiguration.post_login_route = this.oidcConfigService.clientConfiguration.startup_route;
-      // HTTP 403
-      openIDImplicitFlowConfiguration.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
-      // HTTP 401
-      openIDImplicitFlowConfiguration.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
-      openIDImplicitFlowConfiguration.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
-      openIDImplicitFlowConfiguration.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
-      // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
-      // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
-      openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds =
-        this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
+    openIDImplicitFlowConfiguration.stsServer = 'https://identity.ledgerlocal.com';
+    openIDImplicitFlowConfiguration.redirect_url = 'http://www.ledgerlocal.com/#/pages/userdetail';
+    // The Client MUST validate that the aud (audience) Claim contains its client_id value registered at the Issuer identified by the iss (issuer) Claim as an audience.
+    // The ID Token MUST be rejected if the ID Token does not list the Client as a valid audience, or if it contains additional audiences not trusted by the Client.
+    openIDImplicitFlowConfiguration.client_id = 'singleapp';
+    openIDImplicitFlowConfiguration.response_type = 'id_token token';
+    openIDImplicitFlowConfiguration.scope = 'dataEventRecords openid';
+    openIDImplicitFlowConfiguration.post_logout_redirect_uri = 'http://www.ledgerlocal.com';
+    openIDImplicitFlowConfiguration.start_checksession = false;
+    openIDImplicitFlowConfiguration.silent_renew = false;
+    openIDImplicitFlowConfiguration.silent_renew_url = 'https://identity.ledgerlocal.com/silent-renew.html';
+    openIDImplicitFlowConfiguration.post_login_route = '/userdetail';
+    // HTTP 403
+    openIDImplicitFlowConfiguration.forbidden_route = '/Forbidden';
+    // HTTP 401
+    openIDImplicitFlowConfiguration.unauthorized_route = '/Unauthorized';
+    openIDImplicitFlowConfiguration.log_console_warning_active = true;
+    openIDImplicitFlowConfiguration.log_console_debug_active = true;
+    // id_token C8: The iat Claim can be used to reject tokens that were issued too far away from the current time,
+    // limiting the amount of time that nonces need to be stored to prevent attacks.The acceptable range is Client specific.
+    openIDImplicitFlowConfiguration.max_id_token_iat_offset_allowed_in_seconds = 10;
 
-      const authWellKnownEndpoints = new AuthWellKnownEndpoints();
-      authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+    const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+    authWellKnownEndpoints.issuer = 'https://identity.ledgerlocal.com';
 
-      this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
+    authWellKnownEndpoints.jwks_uri = 'https://identity.ledgerlocal.com/.well-known/openid-configuration/jwks';
+    authWellKnownEndpoints.authorization_endpoint = 'https://identity.ledgerlocal.com/connect/authorize';
+    authWellKnownEndpoints.token_endpoint = 'https://identity.ledgerlocal.com/connect/token';
+    authWellKnownEndpoints.userinfo_endpoint = 'https://identity.ledgerlocal.com/connect/userinfo';
+    authWellKnownEndpoints.end_session_endpoint = 'https://identity.ledgerlocal.com/connect/endsession';
+    authWellKnownEndpoints.check_session_iframe = 'https://identity.ledgerlocal.com/connect/checksession';
+    authWellKnownEndpoints.revocation_endpoint = 'https://identity.ledgerlocal.com/connect/revocation';
+    authWellKnownEndpoints.introspection_endpoint = 'https://identity.ledgerlocal.com/connect/introspect';
 
-    });
+    this.oidcSecurityService.setupModule(openIDImplicitFlowConfiguration, authWellKnownEndpoints);
 
   }
 }
