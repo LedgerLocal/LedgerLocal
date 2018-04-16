c425b33407f5dc876d14e049ea6030a88222e6fa
alexandre.lazar <alexandre.lazar@gmail.com>
2018-04-16 12:19:45 +0200

    Fix login screen

--- Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Controllers/MemberManageController.cs
+++ Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Controllers/MemberManageController.cs
@@ -306,6 +306,8 @@ namespace LedgerLocal.IdentityServer.FullNode.Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> LinkLogin(string provider)
         {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
 
             // Request a redirect to the external login provider to link a login for the current user
             var redirectUrl = Url.Action(nameof(LinkLoginCallback), "Manage");
@@ -333,6 +335,8 @@ namespace LedgerLocal.IdentityServer.FullNode.Web.Controllers
             if (result.Succeeded)
             {
                 message = ManageMessageId.AddLoginSuccess;
+                // Clear the existing external cookie to ensure a clean login process
+                await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
             }
             return RedirectToAction(nameof(ManageLogins), new { Message = message });
         }
