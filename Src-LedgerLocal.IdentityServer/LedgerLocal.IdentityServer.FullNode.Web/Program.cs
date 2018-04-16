d374b9faddeb54c4bb4615c6e8d3b198341055cc
alexandre.lazar <alexandre.lazar@gmail.com>
2018-04-16 15:18:38 +0200

    fix path

--- Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Program.cs
+++ Src-LedgerLocal.IdentityServer/LedgerLocal.IdentityServer.FullNode.Web/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -18,8 +19,12 @@ namespace LedgerLocal.IdentityServer.FullNode.Web
 
         public static IWebHost BuildWebHost(string[] args) =>
            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel(o1 => o1.Listen(IPAddress.Loopback, 4444, listenOptions =>
+                {
+                    Console.WriteLine(args[1]);
+                    listenOptions.UseHttps(args[1], args[2]);
+                }))   
+            .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
