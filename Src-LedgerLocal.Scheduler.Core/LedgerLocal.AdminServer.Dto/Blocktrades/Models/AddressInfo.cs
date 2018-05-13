50c285b7f80e3d3876a8b9c94cf5c755372d42d5
alexandre.lazar <alexandre.lazar@gmail.com>
2018-05-14 01:20:35 +0200

    fix build linux

--- Src-LedgerLocal.Scheduler.Core/LedgerLocal.AdminServer.Dto/Blocktrades/Models/AddressInfo.cs
+++ Src-LedgerLocal.Scheduler.Core/LedgerLocal.AdminServer.Dto/Blocktrades/Models/AddressInfo.cs
@@ -15,9 +15,10 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel.DataAnnotations;
+
 using System.Runtime.Serialization;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace IO.Swagger.Models
 { 
