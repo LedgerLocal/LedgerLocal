66bb34b5d0bb42119543b6fc8f7b7d07cb83d1f9
alexandre.lazar <alexandre.lazar@gmail.com>
2018-05-14 02:08:11 +0200

    fix laod config #3

--- Src-LedgerLocal.Scheduler.Core/LedgerLocal.AdminServer.Jobs/KafkaListenerJob.cs
+++ Src-LedgerLocal.Scheduler.Core/LedgerLocal.AdminServer.Jobs/KafkaListenerJob.cs
@@ -11,28 +11,23 @@ namespace LedgerLocal.AdminServer.Jobs
     [DisallowConcurrentExecution]
     public class KafkaListenerJob : IJob
     {
-        private readonly IDbContextService _dbContextService;
         private readonly IKafkaEventService _kafkaMessageService;
 
         private readonly ILogger<KafkaListenerJob> _logger;
 
         public KafkaListenerJob(
                 ILogger<KafkaListenerJob>  logger, 
-                IDbContextService dbContextService, 
                 IKafkaEventService kafkaMessageService
             )
         {
             _logger = logger;
-            _dbContextService = dbContextService;
             _kafkaMessageService = kafkaMessageService;
         }
 
         async Task IJob.Execute(IJobExecutionContext context)
         {
             _logger.LogInformation($"Starting KafkaMessagePooling ...");
-
-            _dbContextService.RefreshFullDomain();
-
+            
             IList<Task> tasks = new List<Task>();
 
             tasks.Add(_kafkaMessageService.PoolMessageFromKafka(TimeSpan.FromMinutes(10)));
